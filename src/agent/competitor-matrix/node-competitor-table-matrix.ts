cef10d9bde3fe87676799ead50dcd944ae94a4d9
PrasanKumar <prasankumar93@gmail.com>
2025-04-01 23:45:38 +0530

    caching  for competitor table matrix

--- src/agent/competitor-matrix/node-competitor-table-matrix.ts
+++ src/agent/competitor-matrix/node-competitor-table-matrix.ts
@@ -6,12 +6,56 @@ import { StringOutputParser } from "@langchain/core/output_parsers";
 
 import { checkErrorToStopWorkflow } from "../error.js";
 import { STEP_EMOJIS } from "../../utils/constants.js";
+import { LoggerCls } from "../../utils/logger.js";
 import { getPromptCompetitorTableMatrix } from "../prompts/prompt-competitor-table-matrix.js";
 import { addSystemMsg } from "../common.js";
 import { getLLM } from "../llms/llm.js";
+import { AgentCache } from "../agent-cache.js";
+
+const updateStateFromCache = async (state: OverallStateType) => {
+  let isCacheHit = false;
+
+  let sortedCompetitorList = [...state.competitorList].sort();
+
+  const agentCache = await AgentCache.getInstance();
+  const cached = await agentCache.getAgentCache({
+    prompt: sortedCompetitorList.join(", "),
+    scope: {
+      feature: state.productFeature,
+      nodeName: "nodeCompetitorTableMatrix",
+    },
+  });
+
+  if (cached) {
+    const response = cached.response;
+    if (response) {
+      isCacheHit = true;
+      state.competitorTableMatrix = response;
+
+      await addSystemMsg(
+        state,
+        "(Cache) Competitor Table Matrix created",
+        STEP_EMOJIS.competitorTable
+      );
+    }
+  }
+
+  return isCacheHit;
+};
 
 const updateState = async (state: OverallStateType, resultStr: any) => {
   if (resultStr) {
+    let sortedCompetitorList = [...state.competitorList].sort();
+    const agentCache = await AgentCache.getInstance();
+    await agentCache.setAgentCache({
+      prompt: sortedCompetitorList.join(", "),
+      response: resultStr,
+      scope: {
+        feature: state.productFeature,
+        nodeName: "nodeCompetitorTableMatrix",
+      },
+    });
+
     await addSystemMsg(
       state,
       "Competitor Table Matrix created",
@@ -25,33 +69,37 @@ const updateState = async (state: OverallStateType, resultStr: any) => {
 };
 
 const nodeCompetitorTableMatrix = async (state: OverallStateType) => {
-  try {
-    const SYSTEM_PROMPT = getPromptCompetitorTableMatrix(state);
+  const isCacheHit = await updateStateFromCache(state);
 
-    const competitorListPrompt = ChatPromptTemplate.fromMessages([
-      ["system", SYSTEM_PROMPT],
-    ]);
+  if (!isCacheHit) {
+    try {
+      const SYSTEM_PROMPT = getPromptCompetitorTableMatrix(state);
 
-    const llm = getLLM();
+      const competitorListPrompt = ChatPromptTemplate.fromMessages([
+        ["system", SYSTEM_PROMPT],
+      ]);
 
-    const outputParser = new StringOutputParser();
+      const llm = getLLM();
 
-    let chain = RunnableSequence.from([
-      competitorListPrompt,
-      llm,
-      outputParser,
-    ]);
+      const outputParser = new StringOutputParser();
 
-    const resultStr = await chain.invoke({
-      ...state,
-    });
+      let chain = RunnableSequence.from([
+        competitorListPrompt,
+        llm,
+        outputParser,
+      ]);
 
-    await updateState(state, resultStr);
-  } catch (err) {
-    state.error = err;
+      const resultStr = await chain.invoke({
+        ...state,
+      });
+
+      await updateState(state, resultStr);
+    } catch (err) {
+      state.error = err;
+    }
+
+    checkErrorToStopWorkflow(state);
   }
-
-  checkErrorToStopWorkflow(state);
   return state;
 };
 
